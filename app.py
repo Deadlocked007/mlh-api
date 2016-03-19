d341d6a484b8b2d590220ced9e3544edbdb4431d
Siraj Zaneer <szaneer@gmail.com>
2016-03-18 21:17:10 -0500

    Images and logo
    2magic4me

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, jsonify
 from bs4 import BeautifulSoup
 import requests, time
-import urllib3
+import urllib
 import datetime
 
 app = Flask(__name__)
@@ -25,32 +25,58 @@ def request_stuff(season, events):
             index = link.find('"')
             link = link[:index]
 
+            images = event_for.find_all('img')
+            counter = 1
+            event_image = ""
+            event_logo = ""
+            for image in images:
+                if counter == 1:
+                    event_image = str(image)
+                    index = event_image.find("src")
+                    event_image = event_image[index:]
+                    index = event_image.find('"') + 1
+                    event_image = event_image[index:]
+                    index = event_image.find('"')
+                    event_image = event_image[:index]
+                    counter += 1
+                else:
+                    event_logo = str(image)
+                    index = event_logo.find("src")
+                    event_logo = event_logo[index:]
+                    index = event_logo.find('"') + 1
+                    event_logo = event_logo[index:]
+                    index = event_logo.find('"')
+                    event_logo = event_logo[:index]
+
             event_head = str(event_for.find_all('h3'))
             index = event_head.find(">") + 1
             event_head = event_head[index:]
             index = event_head.find("<")
             event_head = event_head[:index]
-
+            
             event_date = str(event_for.find_all('p'))
             index = event_date.find(">") + 1
             event_date = event_date[index:]
             index = event_date.find("<")
             event_date = event_date[:index]
-
+            
             event_loc = str(event_for.find_all('p')[1])
             index = event_loc.find(">") + 1
             event_loc = event_loc[index:]
             index = event_loc.find("<")
             event_loc = event_loc[:index]
-
+            
             event = {}
             event["location"] = event_loc
             event["date"] = event_date
             event["name"] = event_head
             event["link"] = link
             event["id"] = event_id
+            event["image"] = event_image
+            event["logo"] = event_logo
             events[event_head] = event
-    
+
+
 @app.route('/')
 def index():
     us_event = {}
@@ -100,7 +126,5 @@ def search_by_key(mlh_event, key_):
                     return eu_event[i][key_]
 
 
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+
+app.run()
