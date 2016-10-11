a93da46e9ca4103220f66c1a87d0e3f69555a185
Siraj Zaneer <szaneer@gmail.com>
2016-10-11 13:53:31 -0500

    ipv6

--- app.py
+++ app.py
@@ -129,5 +129,5 @@ def search_by_key(mlh_event, key_):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    #port = int(os.environ.get('PORT', 5000))
+    app.run()#host='0.0.0.0', port=port)
